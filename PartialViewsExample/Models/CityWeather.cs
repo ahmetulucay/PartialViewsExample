772346fa4add3012d1c3165bd4fc4af1a4843403
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2023-04-12 14:54:35 +0200

    Add project files.

---
+++ PartialViewsExample/Models/CityWeather.cs
@@ -0,0 +1,8 @@
+﻿namespace PartialViewsExample.Models;
+public class CityWeather
+{
+    public string CityUniqueCode { get; set; } = "";
+    public string CityName { get; set; } = "";
+    public DateTime DateAndTime { get; set; }
+    public int TemperatureFahrenheit { get; set; }
+}
